bd20bcd2764d5d097b38ca088b5b27f548e0a04b
Rian Touchent <riantouchent@gmail.com>
2021-12-05 20:59:13 +0100

    fix encoder output shape

--- wrappers/VAE_extractor.py
+++ wrappers/VAE_extractor.py
@@ -1,15 +1,20 @@
 import gym
+import numpy as np
 from pl_bolts.models.autoencoders import VAE
 
 
-class FeatureExtractionWrapper(gym.ObservationWrapper):
+class VAEFeatureExtractionWrapper(gym.ObservationWrapper):
     def __init__(self, env):
         super().__init__(env)
-        model1 = VAE(input_height=224, pretrained='imagenet', input_channels=3, encoder='resnet18')
-        encoder = model1.encoder
+        vae = VAE(input_height=224, pretrained='imagenet',
+                     input_channels=3, encoder='resnet18')
+        encoder = vae.encoder
         encoder.eval()
-        
+
         self.encoder = encoder
+        self.observation_space = gym.spaces.Box(low=np.finfo(np.float32).min,
+                                                high=np.finfo(np.float32).max, 
+                                                shape=(1,vae.enc_out_dim), dtype=np.float32)
 
     def observation(self, obs):
         obs = self.encoder(obs)
