b1fd233a24ae264cb014650798567b8d3c9fc65b
Rian Touchent <riantouchent@gmail.com>
2021-12-05 21:02:01 +0100

    fix codestyle

--- wrappers/VAE_extractor.py
+++ wrappers/VAE_extractor.py
@@ -7,14 +7,16 @@ class VAEFeatureExtractionWrapper(gym.ObservationWrapper):
     def __init__(self, env):
         super().__init__(env)
         vae = VAE(input_height=224, pretrained='imagenet',
-                     input_channels=3, encoder='resnet18')
+                  input_channels=3, encoder='resnet18')
+
         encoder = vae.encoder
         encoder.eval()
 
         self.encoder = encoder
         self.observation_space = gym.spaces.Box(low=np.finfo(np.float32).min,
-                                                high=np.finfo(np.float32).max, 
-                                                shape=(1,vae.enc_out_dim), dtype=np.float32)
+                                                high=np.finfo(np.float32).max,
+                                                shape=(1, vae.enc_out_dim),
+                                                dtype=np.float32)
 
     def observation(self, obs):
         obs = self.encoder(obs)
